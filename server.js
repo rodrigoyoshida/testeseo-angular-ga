97b2d57a6b7c521b66365606e466be1d1a949422
Rodrigo Yoshida <rodrigo.yoshida@focusnetworks.com.br>
2018-01-19 17:53:09 -0200

    Build passando, porém server.js não funciona, nova tentativa

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic('dist')).listen(8080, function(){});
+var express = require('express');
+var app = express();
+
+// Remove/change the port if needed
+app.set('port', (process.env.PORT || 8080 ));
+
+/* Libraries redirects */
+app.use(express.static(__dirname + '/dist'));
+
+app.all('/*', function(req, res, next){
+  res.sendFile('/dist/index.html', {root: __dirname});
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+});
